91d572159ed449f415336ca3f28381ac0ddce343
코마벅스 <comabugs@gmail.com>
2025-02-21 20:46:09 +0900

    log 설정

--- config/settings/base.py
+++ config/settings/base.py
@@ -136,3 +136,44 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+# 로깅 설정
+LOGGING= {
+ 'disable_existing_loggers': False,
+ 'filters': {'require_debug_false': {'()': 'django.utils.log.RequireDebugFalse'},
+             'require_debug_true': {'()': 'django.utils.log.RequireDebugTrue'}},
+ 'formatters': {
+     'django.server': {'()': 'django.utils.log.ServerFormatter',
+                                  'format': '[{server_time}] {message}',
+                                  'style': '{'},
+    'standard' : {
+     'format' : '%(asctime)s [%(levelname)s] %(name)s: %(message)s'
+ },
+                },
+ 'handlers': {'console': {'class': 'logging.StreamHandler',
+                          'filters': ['require_debug_true'],
+                          'level': 'INFO'},
+              'django.server': {'class': 'logging.StreamHandler',
+                                'formatter': 'django.server',
+                                'level': 'INFO'},
+              'mail_admins': {'class': 'django.utils.log.AdminEmailHandler',
+                              'filters': ['require_debug_false'],
+                              'level': 'ERROR'},
+              'file' : {
+                  'level' : 'INFO',
+                  'filter' : ['require_debug_false'],
+                  'class' : 'logging.handlers.RotatingFileHandler',
+                  'filename' : BASE_DIR / 'logs/mysite.log',
+                  'maxBytes' : 1024 * 1024 * 5, # 5MB
+                  'backupCount' : 5,
+                  'formatter' : 'standard',
+              },
+              },
+ 'loggers': {'django': {'handlers': ['console', 'mail_admins', 'file'],
+                        'level': 'INFO'},
+             'django.server': {'handlers': ['django.server'],
+                               'level': 'INFO',
+                               'propagate': False}},
+ 'version': 1,
+
+}
+
