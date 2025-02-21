9a74555114c1949d6bf9bbc052522e533a130bc0
코마벅스 <comabugs@gmail.com>
2025-02-21 20:48:43 +0900

    log 설정2

--- config/settings/base.py
+++ config/settings/base.py
@@ -160,7 +160,7 @@ LOGGING= {
                               'level': 'ERROR'},
               'file' : {
                   'level' : 'INFO',
-                  'filter' : ['require_debug_false'],
+                  'filters' : ['require_debug_false'],
                   'class' : 'logging.handlers.RotatingFileHandler',
                   'filename' : BASE_DIR / 'logs/mysite.log',
                   'maxBytes' : 1024 * 1024 * 5, # 5MB
